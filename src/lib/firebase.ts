614c1e4e0832d77d6a697633144623da1e5564c4
Ayomidejhay <ayomiolaniyan@gmail.com>
2026-05-12 16:11:40 +0100

    add secret keys to env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -35,13 +35,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
  const firebaseConfig = {
-  apiKey: "AIzaSyB7drA_TFIkhswLmmzjzrIn75OtLo0ZCt4",
-  authDomain: "runnars-e518a.firebaseapp.com",
-  projectId: "runnars-e518a",
-  storageBucket: "runnars-e518a.firebasestorage.app",
-  messagingSenderId: "901939206181",
-  appId: "1:901939206181:web:d3734d8f7fbd8b0cde07cc",
-  measurementId: "G-W2TBQK2KL3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
